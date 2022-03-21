3015dc6619661da323772d86e3f63418583b33d4
Isaac Luton <isaacdluton@gmail.com>
2022-03-20 22:08:40 -0700

    bug fixes and improved functionality

--- script.js
+++ script.js
@@ -2,21 +2,48 @@ const button = document.querySelector(".button");
 const questionEL = document.querySelector(".question");
 const choicesEl = document.querySelector(".quizIntro");
 const mainEl = document.querySelector("main");
-const answerOne = document.createElement("h3");
-const answerTwo = document.createElement("h3");
-const answerThree = document.createElement("h3");
-const answerFour = document.createElement("h3");
 const timerEL = document.querySelector(".timer");
 const highScoresEl = document.querySelector(".highScores");
 const quizIntroEl = document.querySelector(".quizIntro");
 const fullScoreEl = document.querySelector(".fullScore");
+const highScoreSubmit = document.getElementById("highSubmit")
+let answersDelete = document.getElementsByTagName("h3");
+let questionIndex = 0;
+let answers = document.querySelector(".answers")
 let scoreEl = document.querySelector(".score");
 let correctAnswer;
 
 let score = 0;
 let secondsLeft = 45;
 
-//timer function
+
+//event listener to start quiz when start is clicked
+button.addEventListener("click", startQuiz)
+
+
+// function to pass to event listener 
+function startQuiz () {
+    setTimer()
+    button.remove();
+    quizIntroEl.remove();
+    showQuestion();
+
+    highScoresEl.style.visibility = "hidden";
+}
+
+// eventlistener to see if high scores is clicked
+
+
+highScoresEl.addEventListener("click", function() {
+
+    questionEL.textContent = ("High Scores")
+    button.remove();
+    quizIntroEl.remove();
+    fullScoreEl.remove();
+
+});
+
+//timer function to start countdown after start is clicked includes if statemoent for when timer hits 0 seconds
 
 function setTimer() {
     
@@ -29,197 +56,172 @@ function setTimer() {
 
         }
 
-        if (secondsLeft === 0) {
+        // show game over after timer hits 0 seconds
 
-            answerOne.remove();
-            answerTwo.remove();
-            answerThree.remove();
-            answerFour.remove();
-        
+        if (secondsLeft < 0) {
+
             questionEL.textContent = ("GameOver")
+            clearPage();
+            scoreStore();
+            highScoresEl.style.visibility = "visible";
+            timeLeft = clearInterval(timerInterval);
+            scoreSubmit();
+            
+
+        }
+
+        if (questionEL.textContent === "Enter Name to Save Score" ) {
+            timeLeft = clearInterval(timerInterval);
         }
     }, 1000)
 }
 
-//array of questions for question set one
+
+//array of questions 
+
+const questions = [
+    {
+        question: "Where should a JavaScript link be placed in yout html?",
+        options: [
+            {text: "In the body </body> before the closing tag"},
+            {text: "In the Head <head> with our CSS"},
+            {text:"In the footer <footer>"},
+            {text:"Anywhere is fine"},
+        ],
+        answer: "In the body </body> before the closing tag"
 
 
-    function firstQuestion() {
-        questionEL.textContent = ("Where should a JavaScript link be placed in yout html?")
-        answerOne.textContent = ("In the body </body> before the closing tag");
-        answerTwo.textContent = ("In the Head <head> with our CSS");
-        answerThree.textContent = ("In the footer <footer>");
-        answerFour.textContent = ("Anywhere is fine");
-        choicesEl.textContent = ("");
-        correctAnswer = answerOne;
+    },
+//question set two
+    {
+        question: "Which of these is a correct JavaScript comment?",
+        options: [
+            {text: "// This is a comment"},
+            {text: "! This is a comment"},
+            {text: "! This is a comment"},
+            {text: "' This is a comment'"}
+        ],
+        answer: "// This is a comment"
+    },
 
-    }
+//question set 3
+    {
+        question: "Which operator is used to assign a value to a variable?",
+        options: [
+            {text: "+"},
+            {text: "=",},
+            {text: "=="},
+            {text: "==="}
+        ],
+        answer: "="
+    },
 
-//function question set two
-
-
-    function secondQuestion() {
-        questionEL.textContent = ("Which of these is a correct JavaScript comment?")
-        answerOne.textContent = ("// This is a comment");
-        answerTwo.textContent = ("! This is a comment");
-        answerThree.textContent = ("* This is a comment");
-        answerFour.textContent = ("' This is a comment'");
-        choicesEl.textContent = ("");
-        correctAnswer = answerOne;
-    }
-
-//function question set 3
-
-    function thirdQuestion() {
-        questionEL.textContent = ("Which operator is used to assign a value to a variable?")
-        answerOne.textContent = ('+');
-        answerTwo.textContent = ("=");
-        answerThree.textContent = ("==");
-        answerFour.textContent = ("===");
-        choicesEl.textContent = ("");
-        correctAnswer = answerTwo;
-    }
-
-// function question set 4
-
-
-    function fourthQuestion() {
-        questionEL.textContent = ("Which built in JavaScript method will remove the last element from an Array?")
-        answerOne.textContent = ('push()');
-        answerTwo.textContent = ("slice()");
-        answerThree.textContent = ("pop()");
-        answerFour.textContent = ("cut()");
-        choicesEl.textContent = ("");
-        correctAnswer = answerThree;
+// question set 4
+    {
+        question: "Which built in JavaScript method will remove the last element from an Array?",
+        options: [
+            {text: "push()"},
+            {text: "slice()"},
+            {text: "pop()", },
+            {text: "cut()", }
+        ],
+        answer: "pop()"
+    },
         
+]
+
+
+//function to get questions
+function showQuestion () {
+    pickQuestion(questions[questionIndex])
+}
+
+//function to get questions
+
+function pickQuestion (question) {
+    questionEL.textContent = question.question;
+    questionEL.style.textAlign = "center";
+    
+    question.options.forEach(answer => {
+        let answerHolder = document.createElement("h3");
+        answerHolder.textContent = answer.text;
+        if (answerHolder.textContent === question.answer) 
+            {answerHolder.classList.add("correct")};
+        mainEl.appendChild(answerHolder);
+        answerHolder.addEventListener("click", isCorrect)
+    });
+}
+
+function isCorrect(e) {
+    let answerPicked = e.target
+
+    let correctAnswer = document.querySelector(".correct");
+    
+    if (answerPicked === correctAnswer) {
+        scorePoints();
+        correctAnswer.style.backgroundColor = "green";
+        setTimeout(() => { correctAnswer.style.backgroundColor = "white";}, 1000);
+        clearPage();
+        nextQuestion();
+
+    } else {
+        e.target.style.backgroundColor = "red";
+        setTimeout(() => { e.target.style.backgroundColor = "white";}, 1000);
+        wrongAnswer();
     }
 
-
-
-// function to add h3 questions to main tag
-function fullQuestion() {
-    mainEl.appendChild(answerOne);
-    mainEl.appendChild(answerTwo);
-    mainEl.appendChild(answerThree);
-    mainEl.appendChild(answerFour);
 }
 
+function nextQuestion () {
+    
+    if (questionIndex < 3) {
+    questionIndex = questionIndex + 1;
+    showQuestion(questions[questionIndex])
+    } else {
+        questionEL.textContent = "Enter Name to Save Score";
+        scoreStore();
+        highScoresEl.style.visibility = "visible";
+        scoreSubmit();
+    }
+}
 
-//function to reset h3 background-color
+function clearPage () {
 
-function colorReset () {
-    answerOne.style.backgroundColor = "white";
-    answerTwo.style.backgroundColor = "white";
-    answerThree.style.backgroundColor = "white";
-    answerFour.style.backgroundColor = "white";
+    for (let index = 0; index <= 3; index++) {
+    mainEl.removeChild(mainEl.lastElementChild);
+    }
 
 }
 
-//function to add 25points to score if correct answer is selected 
+//function to add 10points to score if correct answer is selected 
 
 function scorePoints() {
 
     score += 10;
     scoreEl.textContent = score;
+    scoreEl.style.backgroundColor = "green";
+    setTimeout(() => { scoreEl.style.backgroundColor = "White";}, 1000);
 
 }
 
-//function to remove 10 points
+//function to remove 5 points if wrong answer is picked and deducted 5 seconds from clock
 
 function wrongAnswer() {
 
     score -= 5;
     scoreEl.textContent = score;
+    scoreEl.style.backgroundColor = "red";
+    setTimeout(() => { scoreEl.style.backgroundColor = "White";}, 1000);
     secondsLeft -= 5; 
+    timerEL.style.backgroundColor = "red";
+    setTimeout(() => { timerEL.style.backgroundColor = "rgb(172, 172, 172)";}, 1000);
 }
 
-//function to add event lister to all h3 tags
-
-function answerLister () {
-    answerOne.addEventListener("click", function () {
-
-        if (correctAnswer === answerOne) {
-            answerOne.style.backgroundColor = "green";
-            scorePoints();
-            setTimeout(() => {  fourthQuestion()}, 1000); 
-            setTimeout(() => {  colorReset()}, 1000); 
-        } else {
-            answerOne.style.backgroundColor = "red";
-            wrongAnswer();
-        };
-    });
-    
-
-    answerTwo.addEventListener("click", function () {
-
-        if (correctAnswer === answerOne ) {
-            answerTwo.style.backgroundColor = "green";
-            scorePoints();
-            setTimeout(() => { colorReset()}, 1000);
-            answerOne.remove();
-            answerTwo.remove();
-            answerThree.remove();
-            answerFour.remove();
-        
-            questionEL.textContent = ("GameOver")
-        } else {
-            answerTwo.style.backgroundColor = "red";
-            wrongAnswer();
-        }; 
-    }); 
-
-    answerThree.addEventListener("click", function () {
-
-        if (correctAnswer === answerThree) {
-            answerThree.style.backgroundColor = "green";
-            scorePoints();
-            setTimeout(() => { thirdQuestion()}, 1000);
-            setTimeout(() => { colorReset()}, 1000);
-        } else {
-            answerThree.style.backgroundColor = "red";
-            wrongAnswer();
-        };
+function scoreStore () {
 
     
-    });
+}
 
-    answerFour.addEventListener("click", function () {
-
-        if (correctAnswer === answerTwo) {
-            answerThree.style.backgroundColor = "green";
-            scorePoints();
-            setTimeout(() => { secondQuestion()}, 1000);
-            setTimeout(() => { colorReset()}, 1000);
-        } else {
-            answerThree.style.backgroundColor = "red";
-            wrongAnswer();
-        };
-
-    
-    });
-
-
-
-};
-
-// function to check if answer selected matches right answer
-
-
-
-button.addEventListener("click", function() {
-
-    fullQuestion();
-    firstQuestion();
-    answerLister();
-    button.remove();
-    setTimer();
-})
-
-highScoresEl.addEventListener("click", function() {
-
-    questionEL.textContent = ("High Scores")
-    button.remove();
-    quizIntroEl.remove();
-    fullScoreEl.remove();
-
-});
+function scoreSubmit () {
+    highScoreSubmit.id = "#highSubmitVisible";
+}
